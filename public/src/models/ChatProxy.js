0ad7dcf84804ae63720cfeb1d28e5ee8a5f9f762
mgechev <mgechev@gmail.com>
2014-08-19 19:00:19 +0300

    Update chatproxy

--- public/src/models/ChatProxy.js
+++ public/src/models/ChatProxy.js
@@ -1,6 +1,6 @@
 function ChatProxy() {
   EventEmitter.call(this);
-  this._peers = [];
+  this._peers = {};
 }
 
 ChatProxy.prototype = Object.create(EventEmitter.prototype);
@@ -32,10 +32,18 @@ ChatProxy.prototype.send = function (message) {
 };
 
 ChatProxy.prototype._connectTo = function (username) {
-  var peer = this.peer.connect(username);
-  peer.on('open', function () {
-    this._peers[username] = peer;
+  var conn = this.peer.connect(username);
+  conn.on('open', function () {
+    this._registerPeer(username, conn);
   }.bind(this));
+};
+
+ChatProxy.prototype._registerPeer = function (username, conn) {
+  console.log('Registering', username);
+  this._peers[username] = conn;
+  conn.on('data', function (e) {
+    console.log('Data received', e);
+  });
 };
 
 ChatProxy.prototype._disconnectFrom = function (username) {
@@ -48,23 +56,31 @@ ChatProxy.prototype.connect = function (username) {
   this.socket = io();
   this.socket.on('connect', function () {
     self.socket.on(Topics.USER_CONNECTED, function (userId) {
+      if (userId === self.getUsername()) {
+        return;
+      }
       self._connectTo(userId);
       self.emit(Topics.USER_CONNECTED, userId);
+      console.log('User connected', userId);
     });
     self.socket.on(Topics.USER_DISCONNECTED, function (userId) {
+      if (userId === self.getUsername()) {
+        return;
+      }
       self._disconnectFrom(userId);
       self.emit(Topics.USER_DISCONNECTED, userId);
+      console.log('User disconnected', userId);
     });
   });
   console.log('Connecting with username', username);
   this.peer = new Peer(username, {
     host: 'localhost', port: 9000, path: '/chat'
   });
-  this.peer.on('open', function (e) {
-    console.log(e, arguments);
+  this.peer.on('open', function (userId) {
+    self.setUsername(userId);
   });
-  this.peer.on('connection', function (e) {
-    console.log(e);
+  this.peer.on('connection', function (conn) {
+    self._registerPeer(username, conn);
   });
 };
 
