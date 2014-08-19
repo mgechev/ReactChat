a37a64c59af54ecd488e731092e7e8b536cbc8e6
mgechev <mgechev@gmail.com>
2014-08-19 19:54:29 +0300

    Remove useless collection

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var PeerServer = require('peer').PeerServer,
     express = require('express'),
     Topics = require('./public/src/Topics.js'),
     app = express(),
-    port = process.env.PORT || 3001,
-    users = [];
+    port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,13 +15,11 @@ console.log('Listening on port', port);
 var peerServer = new PeerServer({ port: 9000, path: '/chat' });
 
 peerServer.on('connection', function (id) {
-  users.push(id);
   io.emit(Topics.USER_CONNECTED, id);
   console.log('User connected with #', id);
 });
 
 peerServer.on('disconnect', function (id) {
-  users.splice(users.indexOf(id), 1);
   io.emit(Topics.USER_DISCONNECTED, id);
   console.log('User disconnected with #', id);
 });
