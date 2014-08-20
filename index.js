4c26346d38bc22f12c8d685a130a5fa208e20342
mgechev <mgechev@gmail.com>
2014-08-20 05:20:26 +0300

    Dummy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var io = require('socket.io').listen(expressServer);
 
 console.log('Listening on port', port);
 
-var peerServer = new PeerServer({ port: 443, path: '/chat' });
+var peerServer = new PeerServer({ port: 9000, path: '/chat' });
 
 peerServer.on('connection', function (id) {
   io.emit(Topics.USER_CONNECTED, id);
